307ada508bd0d6dc672a7e406547769739764dfc
JHex-git <JHex_email@163.com>
2022-07-09 08:44:37 +0800

    fix: dynamic stove

--- src/viewmodel/sprite/food/stove.cpp
+++ src/viewmodel/sprite/food/stove.cpp
@@ -8,6 +8,7 @@ const int DELTA_X = 20;
 const int DELTA_Y = 62;
 const int FUNC_START_INDEX = 12;
 const int FUNC_TAKE_EFFECT_INDEX = 26;
+const float FRAME_RATE = 1;
 const int LEFT_PADDING = 20;
 const int RIGHT_PADDING = 20;
 
@@ -20,7 +21,6 @@ bool Stove::CheckFunc()
 
 void Stove::Func()
 {
-    DEBUG_INFO("Generate");
     flame_manager->GenerateFlame(center_x + FLAME_PADDING_X, center_y + FLAME_PADDING_Y);
 }
 
@@ -28,4 +28,4 @@ Stove::Stove(int x, int y, float frame_rate, FlameManager *flame_manager) :
     Food(x, y, HEALTH, frame_rate, COOL_DOWN_TIME), flame_manager(flame_manager) {}
 
 Stove::Stove(int x, int y, int row, int column, std::vector<QPixmap *> frames, FlameManager *flame_manager) :
-    Food(x, y, DELTA_X, DELTA_Y, row, column, LEFT_PADDING, RIGHT_PADDING, frame_rate, frames, FUNC_START_INDEX, FUNC_TAKE_EFFECT_INDEX, FLAME_START, HEALTH, COOL_DOWN_TIME), flame_manager(flame_manager) {}
+    Food(x, y, DELTA_X, DELTA_Y, row, column, LEFT_PADDING, RIGHT_PADDING, FRAME_RATE, frames, FUNC_START_INDEX, FUNC_TAKE_EFFECT_INDEX, FLAME_START, HEALTH, COOL_DOWN_TIME), flame_manager(flame_manager) {}
