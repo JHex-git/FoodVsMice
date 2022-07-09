23acfebd24a070cd4a67318c432742f3d59d03af
JHex-git <JHex_email@163.com>
2022-07-09 14:51:21 +0800

    last version

--- src/viewmodel/card/egg_card.cpp
+++ src/viewmodel/card/egg_card.cpp
@@ -3,7 +3,7 @@
 const int DELTA_X = -10;
 const int DELTA_Y = 23;
 const float COOLDOWN_TIME = 3;
-const int FLAME_COST = 50;
+const int FLAME_COST = 80;
 
 EggCard::EggCard(FoodType food_type, int x, int y, QPixmap *img)
     : Card(food_type, x, y, DELTA_X, DELTA_Y, img, COOLDOWN_TIME, FLAME_COST) {}
--- src/viewmodel/card/steamdrawer_card.cpp
+++ src/viewmodel/card/steamdrawer_card.cpp
@@ -3,7 +3,7 @@
 const int DELTA_X = -28;
 const int DELTA_Y = -17;
 const float COOLDOWN_TIME = 5;
-const int FLAME_COST = 80;
+const int FLAME_COST = 50;
 
 SteamDrawerCard::SteamDrawerCard(FoodType food_type, int x, int y, QPixmap *img)
     : Card(food_type, x, y, DELTA_X, DELTA_Y, img, COOLDOWN_TIME, FLAME_COST) {}
